ce82dc32b1ffe33ca6a135f34e35fe928f43c0c1
Sam Lawton <sam.lawton@wwt.com>
2015-12-11 14:11:34 -0600

    feat(MonsterSvc): add remaining MonsterSvc methods
    Add the getMonstersById function
    Add the updateMonster function
    Refactor find calls into findById-style calls
    Add endpoints for Monster PUT and GET (by id)
    Update tests where new (and old) functions require them

--- server/monster/MonsterSvc.js
+++ server/monster/MonsterSvc.js
@@ -15,19 +15,31 @@ function getMonsters() {
     return Monster.find({});
 }
 
+function getMonsterById(req) {
+    return Monster.findById(req.params.monster_id);
+}
+
 function addMonster(req) {
     return Monster.create(req.body);
 }
 
-function removeMonster(req) {
-    return Monster.remove({_id: req.params.monster_id}).then(
-        function () {
-            return {
-                message: "Deleted user with id " + req.params.monster_id
-            };
+function updateMonster(req) {
+    return Monster.findById(req.params.monster_id)
+        .then(function (monster) {
+            _.assign(monster, req.body);
+            monster.save();
+            return monster;
         });
 }
 
+function removeMonster(req) {
+    return Monster.findByIdAndRemove(req.params.monster_id).then(function () {
+        return { message: "Deleted monster with id " + req.params.monster_id };
+    });
+}
+
 exports.getMonsters = createResponder(getMonsters);
+exports.getMonsterById = createResponder(getMonsterById);
 exports.postMonsters = createResponder(addMonster);
+exports.updateMonster = createResponder(updateMonster);
 exports.deleteMonster = createResponder(removeMonster);
